fd286f0affccdd5894a8adebe29e76bdef299417
eicheich <nengindah41@gmail.com>
2023-12-08 21:42:43 +0700

    yes

--- app/Http/Controllers/WisataController.php
+++ app/Http/Controllers/WisataController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\wisata;
 use Illuminate\Http\Request;
 
 class WisataController extends Controller
 {
     public function index()
     {
-        $wisata = \App\Models\wisata::all();
+        $wisata = wisata::all();
         return response()->json([
             'success' => true,
             'message' => 'Daftar data wisata',
@@ -16,50 +17,50 @@ class WisataController extends Controller
         ], 200);
     }
 
-    public function create(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'title' => 'required',
-            'thumbnail' => 'required',
-            'rate' => 'required',
-            'location' => 'required',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        }
-
-        $wisata = \App\Models\wisata::create([
-            'title' => $request->title,
-            'thumbnail' => $request->thumbnail,
-            'rate' => $request->rate,
-            'location' => $request->location,
-        ]);
-
-        if ($wisata) {
-            return response()->json([
-                'success' => true,
-                'message' => 'wisata berhasil ditambahkan',
-                'data' => $wisata
-            ], 201);
-        } else {
-            return response()->json([
-                'success' => false,
-                'message' => 'wisata gagal ditambahkan',
-            ], 409);
-        }
-    }
-
-    public function show($id)
-    {
-        $wisata = \App\Models\wisata::where('id', $id)->first();
-
-        return response()->json([
-            'success' => true,
-            'message' => 'Detail Data wisata',
-            'data' => $wisata
-        ], 200);
-    }
+//    public function create(Request $request)
+//    {
+//        $validator = Validator::make($request->all(), [
+//            'title' => 'required',
+//            'thumbnail' => 'required',
+//            'rate' => 'required',
+//            'location' => 'required',
+//        ]);
+//
+//        if ($validator->fails()) {
+//            return response()->json($validator->errors(), 400);
+//        }
+//
+//        $wisata = \App\Models\wisata::create([
+//            'title' => $request->title,
+//            'thumbnail' => $request->thumbnail,
+//            'rate' => $request->rate,
+//            'location' => $request->location,
+//        ]);
+//
+//        if ($wisata) {
+//            return response()->json([
+//                'success' => true,
+//                'message' => 'wisata berhasil ditambahkan',
+//                'data' => $wisata
+//            ], 201);
+//        } else {
+//            return response()->json([
+//                'success' => false,
+//                'message' => 'wisata gagal ditambahkan',
+//            ], 409);
+//        }
+//    }
+//
+//    public function show($id)
+//    {
+//        $wisata = \App\Models\wisata::where('id', $id)->first();
+//
+//        return response()->json([
+//            'success' => true,
+//            'message' => 'Detail Data wisata',
+//            'data' => $wisata
+//        ], 200);
+//    }
 
 //    public function update(Request $request, $id)
 //    {
--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/wisata", "App\Http\Controllers\wisataController@index");
+Route::get("/wisata", [\App\Http\Controllers\WisataController::class, 'index']);
 //cok
