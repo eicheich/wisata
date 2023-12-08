2b0392270b41787085d4573c4f6dcdab9bb06b60
eicheich <nengindah41@gmail.com>
2023-12-08 17:21:46 +0700

    first commit

---
+++ app/Http/Controllers/WisataController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WisataController extends Controller
+{
+    public function index()
+    {
+        $wisata = \App\Models\wisata::all();
+        return response()->json([
+            'success' => true,
+            'message' => 'Daftar data wisata',
+            'data' => $wisata
+        ], 200);
+    }
+
+    public function create(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'thumbnail' => 'required',
+            'rate' => 'required',
+            'location' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $wisata = \App\Models\wisata::create([
+            'title' => $request->title,
+            'thumbnail' => $request->thumbnail,
+            'rate' => $request->rate,
+            'location' => $request->location,
+        ]);
+
+        if ($wisata) {
+            return response()->json([
+                'success' => true,
+                'message' => 'wisata berhasil ditambahkan',
+                'data' => $wisata
+            ], 201);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'wisata gagal ditambahkan',
+            ], 409);
+        }
+    }
+
+    public function show($id)
+    {
+        $wisata = \App\Models\wisata::where('id', $id)->first();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Detail Data wisata',
+            'data' => $wisata
+        ], 200);
+    }
+
+//    public function update(Request $request, $id)
+//    {
+//        $validator = Validator::make($request->all(), [
+//            'title' => 'required',
+//            'thumbnail' => 'required',
+//            'rate' => 'required',
+//            'location' => 'required',
+//        ]);
+//
+//        if ($validator->fails()) {
+//            return response()->json($validator->errors(), 400);
+//        }
+//
+//        $wisata = \App\Models\wisata::where('id', $id)->first();
+//
+//        if ($wisata) {
+//            $wisata->update([
+//                'title' => $request->title,
+//                'thumbnail' => $
+}
---
+++ app/Models/wisata.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class wisata extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'thumbnail',
+        'rate',
+        'location',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+
+}
